2dc504e7df861bb0b9894097c653e54acbd15935
david586 <d.harding1020@gmail.com>
2024-01-11 06:02:29 +0700

    ticker change

--- src/main.js
+++ src/main.js
@@ -45,7 +45,7 @@ export async function executeScheduledTask(request, env, ctx, usingDemoAccount) 
             }
 
             if (difference <= 0) {
-                if (instrument !== 'EU Stocks 50' && instrument !== 'Alphabet Inc - A (All Sessions)') {
+                if (instrument !== 'EU Stocks 50') {
                     // If so, push the current position to the positionsForClosure array
                     positionsForClosure.push(positions[i]);
                 }
